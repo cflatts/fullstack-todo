9a1dd54a3bc4ce0a33e5b42ed3ef5a01103ac527
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-13 14:30:17 -0500

    set routes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,11 +8,13 @@ const app = function() {
     var ListModel = Backbone.Model.extend ({
         defaults: {
             status: 'incomplete'
+            url: '/api/tasks'
         }
     })
 
     var ListCollection = Backbone.Collection.extend ({
         model: ListModel
+        url: '/api/tweets'
     })
 
     ReactDOM.render(<ListView listColl = {new ListCollection()} />, document.querySelector('.container'))
